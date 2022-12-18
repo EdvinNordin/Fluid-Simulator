e41234265be9f40f59675d145b9aa638188ce9eb
EdvinNordin <nordin.edvin@gmail.com>
2022-12-18 23:05:43 +0100

    for presentation

--- Assets/Scripts/CPU/Points.cs
+++ Assets/Scripts/CPU/Points.cs
@@ -6,50 +6,67 @@ public class Points : MonoBehaviour
 {
 
     List<GameObject> pointObjects;
+
     public GameObject prefab;
     public Material waterMaterialRef;
-    public Color defaultColor;
-    public int debug;
+    public bool debug;
+    public int N;
+    public int iter;
+    public float dt;
+    public float diffusion;
+    public float viscosity;
+
     Fluid fluid;
-    int N = 100;
-    int iter = 16;
+    Color defaultColor;
     Vector3 mousePos, Pos = new Vector3(0,0,0);
     float xPos, yPos;
-    float density = 500;
-    float fadeAmount = 0.0f;
+    float density = 0;
     float prevX, prevY, randX, randY, temp = 0f;
     GameObject Triangle;
     // Start is called before the first frame update
     void Start()
     {
         Camera.main.orthographicSize = N/2;
-        Camera.main.transform.position = new Vector3(0, -0.5f, -10);
+        Camera.main.transform.position = new Vector3(0, -0.5f, -100);
+        Camera.main.orthographic = false;
+        Camera.main.fieldOfView = 54.0f;
+        Camera.main.farClipPlane = 110.0f;
 
         pointObjects = new List<GameObject>();
 
         //dt, diffusion (mixing), viscosity
-        fluid = new Fluid(0.1f, 0.0000f, 0.00f, iter, N);
+        fluid = new Fluid(dt, diffusion, viscosity, iter, N);
+
         //fluid.addDensity(N / 2, N / 2, 700, N);
+        if (debug)
+        {
+            prefab.GetComponent<SpriteRenderer>().enabled = false;
+            prefab.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
+        }
+
 
         for (int i = 0; i < N; i++)
         {
             for (int j = 0; j < N; j++)
             {
-                temp = Mathf.PerlinNoise(i * 10f / (N + 1f), j * 10f / (N + 1f)) * 2 - 1;
+                //temp = Mathf.PerlinNoise(i * 10f / (N + 1f), j * 10f / (N + 1f)) * 2 - 1;
                 //Debug.Log(temp);
                 //fluid.addDensity(i, j, 10*temp, N);
+                
+                
 
                 pointObjects.Add(Instantiate(prefab, new Vector3(i-N / 2, j-N / 2, 0), Quaternion.identity));
+
+                
+
             }
         }
     }
     // Update is called once per frame
     void Update()
     {
-        //fluid.addDensity(N/2, N/2, 10000, N);
         xPos = Mathf.Lerp(-N, N, Mathf.InverseLerp(0, 1100, Input.mousePosition.x)) + N / 2;
         yPos = Mathf.Lerp(-N / 2, N / 2, Mathf.InverseLerp(0, 510, Input.mousePosition.y)) + N / 2;
-        //Debug.Log(xPos);
 
         Color color = Color.white;
 
@@ -62,16 +79,10 @@ public class Points : MonoBehaviour
         {
             for (int j = 0; j < N; j++)
             {
-                //fluid.addVelocity(i, j, 0f,-0.001f,  N);
 
                 if (Input.GetMouseButton(0))
                 {
                     fluid.addDensity((int)yPos, (int)xPos, 1.001f, N);
-                    //fluid.addDensity(N - 1, N /2, 10.001f, N);
-                    /*fluid.addDensity((int)yPos+1, (int)xPos, 0.1f, N);
-                    fluid.addDensity((int)yPos-1, (int)xPos, 0.1f, N);
-                    fluid.addDensity((int)yPos, (int)xPos+1, 0.1f, N);
-                    fluid.addDensity((int)yPos, (int)xPos-1, 0.1f, N);*/
                 }
 
                 Triangle = pointObjects[j + i * N].transform.GetChild(0).gameObject;
@@ -80,7 +91,6 @@ public class Points : MonoBehaviour
 
                 Pos = pointObjects[j + i * N].transform.position;
                 pointObjects[j + i * N].transform.position = new Vector3(Pos.x, Pos.y, -density);
-                //density = fluid.fadeDensity(j + i * N, fadeAmount);
                 color = new Color(density, density, density) * waterMaterialRef.color + defaultColor;
                 pointObjects[j + i * N].GetComponent<SpriteRenderer>().material.color = color;
             }
